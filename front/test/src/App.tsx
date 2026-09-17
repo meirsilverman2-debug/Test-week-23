c48d640ba3f2be50d73204b11cee47f965841901
Meir925man <meirsilverman2@gmail.com>
2026-09-17 10:37:24 +0300

    add: added the three diffrent pages in our web with their routes and the empty for now components

--- front/test/src/App.tsx
+++ front/test/src/App.tsx
@@ -4,6 +4,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router'
+import Signin from './pages/Signin'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +14,10 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
+      <Route path='/' element={<Signin/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='*' element={"404 Not Found"}/>
     </Routes>
     </>
   )
