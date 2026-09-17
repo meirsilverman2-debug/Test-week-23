fb63fa19d3277bb56b6a581015544df40a7c9fc9
Meir925man <meirsilverman2@gmail.com>
2026-09-17 09:44:25 +0300

    add: added the base outline of the front gfile structure with all of the installtion needed

---
+++ front/test/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Routes>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
